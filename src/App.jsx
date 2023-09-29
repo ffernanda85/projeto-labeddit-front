96dc14fcd9946086a5b74d51042a93ecee72f0a9
ffernanda85 <f.fernanda85@gmail.com>
2023-09-29 07:54:01 -0300

    feat: finished comment layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { GlobalContext } from './context/GlobalContext'
-import { GlobalState } from './context/GlobalState';
+import { GlobalContext } from "./context/GlobalContext";
+import { GlobalState } from "./context/GlobalState";
 import { Router } from "./routes/Router";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./styles";
 
 function App() {
-  
-  const context = GlobalState()
+  const context = GlobalState();
 
   return (
     <GlobalContext.Provider value={context}>
-      <Router />
+      <ChakraProvider theme={theme}>
+        <Router />
+      </ChakraProvider>
     </GlobalContext.Provider>
-  )
+  );
 }
 
 export default App;
