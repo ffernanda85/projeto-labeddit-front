c00176efff85e0ac81a3b90365ee24c42e48e0fa
ffernanda85 <f.fernanda85@gmail.com>
2023-09-30 19:27:51 -0300

    feat: render posts

--- src/components/cards/post/cardPosts/CardViewPosts.jsx
+++ src/components/cards/post/cardPosts/CardViewPosts.jsx
@@ -2,29 +2,30 @@ import { useNavigate } from "react-router-dom";
 import * as s from "./styledCardView";
 import { goToComments } from "../../../../routes/coordinator";
 
-export const CardViewPosts = () => {
+export const CardViewPosts = ({ post }) => {
 
   const navigate = useNavigate()
 
+  console.log(post);
+
   return (
     <s.ContainerPost>
-      <s.UserName>Enviado por: creator_id</s.UserName>
+      <s.UserName>Enviado por: {post.creator.name}</s.UserName>
       
       <s.ContentPost>
-        Porque a maioria dos desenvolvedores usam Linux? ou as empresa de
-        tecnologia usam Linux?
+        {post.content}
       </s.ContentPost>
       
       <s.ContainerLikeDislikeComments>
         <s.LikeDislike>
           <img src="https://uploaddeimagens.com.br/images/004/619/369/full/Vector.png?1695754080" alt="like" />
-          <p>1.2k</p>
+          <p>{post.likes - post.dislikes}</p>
           <img src="https://uploaddeimagens.com.br/images/004/619/365/full/Vector_%281%29.png?1695754034" alt="dislike" />
         </s.LikeDislike>
         
         <s.Comments>
           <img src="https://uploaddeimagens.com.br/images/004/619/362/full/fluent_comment-20-regular.png?1695753984" alt="comment" onClick={()=> goToComments(navigate)} />
-          <p>135</p>
+          <p>{post.comments}</p>
         </s.Comments>
       </s.ContainerLikeDislikeComments>
     
--- src/pages/post/PostPage.jsx
+++ src/pages/post/PostPage.jsx
@@ -1,24 +1,36 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CardCreatePost } from "../../components/cards/post/cardCreatePost/CardCreatePost";
 import { CardViewPosts } from "../../components/cards/post/cardPosts/CardViewPosts";
 import { Header } from "../../components/header/Header";
 import * as s from "./stylePost";
+import axios from "axios";
+import { GlobalContext } from "../../context/GlobalContext";
+import { BASE_URL } from "../../constants/constants";
 
 export function PostPage() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const context = useContext(GlobalContext);
 
-  function showPosts() {
-    
+  async function showPosts() {
+    const token = context.getToken();
+    const PATH = BASE_URL + "/posts";
+    const headers = { headers: { Authorization: token } };
+    const result = await axios.get(PATH, headers);
+    setPosts(result.data);
   }
+
+  useEffect(() => {
+    showPosts();
+  }, []);
 
   return (
     <s.Container>
       <Header />
-      < CardCreatePost />
+      <CardCreatePost />
       {
-        posts.map(post => {
-          < CardViewPosts key={post.id} post={post} />
-        })
+        posts?.map((post) =>
+          <CardViewPosts key={post.id} post={post} />
+        )
       }
     </s.Container>
   );
