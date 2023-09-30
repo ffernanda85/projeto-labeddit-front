aa228c8ae7442f20a25af835b350721352e5eba5
ffernanda85 <f.fernanda85@gmail.com>
2023-09-30 19:39:09 -0300

    adjusted styled

--- src/pages/post/stylePost.jsx
+++ src/pages/post/stylePost.jsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
+  min-height: 100vh;
+  max-width: 100vw;
   display: flex;
   flex-direction: column;
   gap: 1rem;
   align-items: center;
 `;
-
