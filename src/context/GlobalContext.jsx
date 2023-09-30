f0a554caadcebe6aa8c4dfbf527f6aef93a8443f
ffernanda85 <f.fernanda85@gmail.com>
2023-09-30 14:41:30 -0300

    feat: created ProtectedRouter

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,3 +1,15 @@
 import { createContext } from "react";
 
 export const GlobalContext = createContext()
+
+export function GlobalState({ children }) {
+    
+    
+    const context = {}
+
+    return (
+        <GlobalContext.Provider value={context}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,18 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { LoginPage } from '../pages/login/LoginPage'
-import { SignupPage } from '../pages/signup/SignupPage'
-import { PostPage } from '../pages/post/PostPage'
-import { CommentPage } from '../pages/comment/CommentPage'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "../pages/login/LoginPage";
+import { SignupPage } from "../pages/signup/SignupPage";
+import { PostPage } from "../pages/post/PostPage";
+import { CommentPage } from "../pages/comment/CommentPage";
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 export function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={ <LoginPage/> } />
-                <Route path='/signup' element={ <SignupPage/> } />
-                <Route path='/posts' element={ < PostPage/> } />
-                <Route path='/comments' element={ < CommentPage/> } />
-            </Routes>
-        </BrowserRouter>
-    )
+  const context = useContext(GlobalContext);
+
+  const ProtectedRouter = ({ children, redirect }) => {
+    const logged = context.isLogged;
+    return logged ? children : <Navigate to={redirect} />;
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        <Route
+          path="/posts"
+          element={
+            <ProtectedRouter redirect={"/"}>
+              <PostPage />
+            </ProtectedRouter>
+          }
+        />
+        <Route
+          path="/comments"
+          element={
+            <ProtectedRouter redirect={"/"}>
+              <CommentPage />
+            </ProtectedRouter>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
